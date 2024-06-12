09c5cfe95952e506e8da46bc2ed063fdf74c0b3b
Leda Wolf <leda@hey.com>
2024-06-12 21:59:17 +0200

    Add useful modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/sanity", "@nuxt/eslint"], 
+  modules: ["@nuxtjs/sanity", "@nuxt/eslint", "@vueuse/nuxt"],
   sanity: {
-    
-  }
-})
+    projectId: process.env.NUXT_SANITY_PROJECT_ID,
+    dataset: process.env.NUXT_SANITY_DATASET,
+  },
+});
