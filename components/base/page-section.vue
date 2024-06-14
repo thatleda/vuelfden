5ebde473d75c271a3cf4f5cbe8b60f64ca125220
Leda Wolf <leda@hey.com>
2024-06-14 19:49:12 +0200

    Add page sections

---
+++ components/base/page-section.vue
@@ -0,0 +1,62 @@
+<template>
+  <transition-animation
+    :animation-duration="props.animationProps?.animationDuration ?? '600ms'"
+    :animation-name="props.animationProps?.animationName ?? 'fade-up'"
+    :animation-delay="props.animationProps?.animationDelay"
+    :animation-fill-mode="props.animationProps?.animationFillMode"
+    :animation-timing="props.animationProps?.animationTiming"
+  >
+    <section :id="props.anchor" :class="$style.section">
+      <div :class="$style.wrapper">
+        <h2 v-if="props.heading">{{ props.heading }}</h2>
+        <div :class="$style.cards"><slot /></div>
+      </div>
+    </section>
+  </transition-animation>
+</template>
+
+<script setup lang="ts">
+import type { AnimationProps } from "./transition-animation.vue";
+
+import TransitionAnimation from "./transition-animation.vue";
+
+interface SectionProps {
+  anchor?: string;
+  animationProps?: AnimationProps;
+  heading?: null | string;
+}
+
+const props = defineProps<SectionProps>();
+</script>
+
+<style module>
+.section {
+  height: auto;
+  background: var(--background-color);
+  scroll-margin-top: 60px;
+  padding: var(--page-padding);
+  margin: 0 auto 4rem auto;
+
+  @media (max-width: 749px) {
+    margin: 0 auto 2rem auto;
+    padding-bottom: 2rem;
+  }
+}
+
+.wrapper {
+  height: 100%;
+  max-width: var(--page-width);
+  margin: 0 auto 0 auto;
+  padding: var(--page-padding);
+
+  @media (max-width: 749px) {
+    padding: 0;
+  }
+}
+
+.cards {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,17 @@
 <template>
-  <transition-animation>
+  <page-section anchor="who" heading="Who?">
     <about-leda
       :content="aboutPage.content"
       :image-id="aboutPage.banner.asset._ref"
     />
-  </transition-animation>
-  <h2>{{ previouslyPage.title }}</h2>
-  <sanity-content :blocks="previouslyPage.content" />
+  </page-section>
+  <page-section :heading="previouslyPage.title" anchor="previously">
+    <div><sanity-content :blocks="previouslyPage.content" /></div>
+  </page-section>
 </template>
 
 <script setup>
-import transitionAnimation from "~/components/base/transition-animation.vue";
+import pageSection from "~/components/base/page-section.vue";
 
 const query = groq`*[_type == "page" && slug.current == $slug][0]`;
 const { data: previouslyPage } = useSanityQuery(query, { slug: "previously" });
