d0dee1933de157838c384e327d9e08ba9099f044
Leda Wolf <leda@hey.com>
2024-06-29 10:28:22 +0200

    Try staticly rendering blog index page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,7 +25,7 @@ export default defineNuxtConfig({
     '/': { swr: true },
     '/imprint': { prerender: true },
     '/privacy': { prerender: true },
-    '/ramblings': { swr: true },
+    '/ramblings': { prerender: true },
     '/ramblings/**': { prerender: true },
   },
   sanity: {
