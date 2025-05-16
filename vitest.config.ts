924655859138d723fc3900c308081a2303ae279b
Leda Wolf <leda@hey.com>
2025-05-16 16:14:05 +0200

    Add include option

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,6 @@ export default defineVitestConfig({
     environment: 'happy-dom',
     globals: true,
     setupFiles: ['./vitest.setup.ts'],
+    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 })
