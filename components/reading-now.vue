f886037b1c19221113772bb1985a6c06c1c553e1
Leda Wolf <leda@hey.com>
2025-10-12 17:18:35 +0100

    Add review integration

--- components/reading-now.vue
+++ components/reading-now.vue
@@ -1,25 +1,48 @@
 <template>
-  <div :class="$style.card">
-    <div :class="$style.description">
-      Probably reading
-      <b :class="$style.title"> {{ book.title }} ({{ book.release }})</b>
-      <sub :class="$style.author">
-        by <em>{{ book.author }}</em>
-      </sub>
-      <div v-if="challenge" :class="$style.progress">
-        <p>
-          {{ challenge.description }}: {{ challenge.progress }} of {{ challenge.goal }} books completed
-        </p>
-        <progress :class="$style.progressBar" :max="challenge.goal" :value="challenge.progress" />
+  <div>
+    <div :class="$style.card">
+      <div :class="$style.description">
+        Probably reading
+        <b :class="$style.title"> {{ book.title }} ({{ book.release }})</b>
+        <sub :class="$style.author">
+          by <em>{{ book.author }}</em>
+        </sub>
+        <div v-if="challenge" :class="$style.progress">
+          <p>
+            {{ challenge.description }}: {{ challenge.progress }} of {{ challenge.goal }} books completed
+          </p>
+          <progress :class="$style.progressBar" :max="challenge.goal" :value="challenge.progress" />
+        </div>
+      </div>
+      <img
+        :alt="`The book cover of ${book.title} by ${book.author}`"
+        :src="book.cover || '/images/book-cover-placeholder.png'"
+        :class="$style.cover"
+        loading="lazy"
+        width="300"
+      >
+    </div>
+
+    <div v-if="latestReview">
+      <h3>Latest review</h3>
+      <div :class="$style.card">
+        <img
+          :alt="`The book cover of ${latestReview.book.title}`"
+          :src="latestReview.book.cover"
+          loading="lazy"
+          width="150"
+          :class="$style.cover"
+        >
+        <div :class="$style.description">
+          <span><b>{{ latestReview.book.title }} ({{ latestReview.book.release }})</b> by {{ latestReview.book.author }}</span>
+          <span>Rating: {{ latestReview.rating }} / 5</span>
+          <b v-if="latestReview.hasSpoilers">
+            Contains spoilers
+          </b>
+          <div v-else v-html="latestReview.html || latestReview.text" />
+        </div>
       </div>
     </div>
-    <img
-      :alt="`The book cover of ${book.title} by ${book.author}`"
-      :src="book.cover || '/images/book-cover-placeholder.png'"
-      :class="$style.cover"
-      loading="lazy"
-      width="300"
-    >
   </div>
 </template>
 
@@ -37,6 +60,24 @@ const book = computed(() => {
     release: userBook?.book.release_year ?? 'year unknown',
   }
 })
+
+const reviews = computed(() => {
+  const userReviews = data.me[0]?.goals[0]?.user.reviews ?? []
+  return userReviews.map(review => ({
+    book: {
+      title: review.book.title ?? 'Unknown Title',
+      author: review.book.contributions[0]?.author?.name ?? 'Unknown Author',
+      cover: review.book.image?.url,
+      release: review.book.release_year ?? 'year unknown',
+    },
+    rating: review.rating,
+    text: review.review ?? '',
+    html: review.review_html,
+    hasSpoilers: review.review_has_spoilers,
+  }))
+})
+
+const latestReview = computed(() => reviews.value[0])
 </script>
 
 <style module>
@@ -52,10 +93,10 @@ const book = computed(() => {
 }
 
 .cover {
-  min-width: 180px;
   @media (max-width: 600px) {
     margin: 0 auto;
   }
+  object-fit: cover
 }
 
 .description {
@@ -89,12 +130,5 @@ const book = computed(() => {
 
 .author {
   padding-top: 0.5rem;
-}
-
-.links {
-  display: flex;
-  justify-content: end;
-  align-items: center;
-  gap: 1rem;
 }
 </style>
