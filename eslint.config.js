f0d2e8e0718499a2b5b8b06ce91543a40e9a73d8
Leda Wolf <leda.wolf@di-unternehmer.com>
2026-05-08 17:47:37 +0200

    style: standardize quotes in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,28 +4,28 @@ import testingLibrary from 'eslint-plugin-testing-library'
 
 const noCommentsRule = {
   meta: {
-    type: "problem",
+    type: 'problem',
     docs: {
-      description: "Comments are not allowed in this project as they can cause unnecessary noise or leak into production code. Check configuration to see the exceptions from this rule.",
+      description: 'Comments are not allowed in this project as they can cause unnecessary noise or leak into production code. Check configuration to see the exceptions from this rule.',
     },
-    fixable: "code",
+    fixable: 'code',
   },
   create(context) {
     const { sourceCode } = context
     function processComment(comment) {
-      if (comment.type === "Shebang")
+      if (comment.type === 'Shebang')
         return
       const options = context.options[0] || {}
       const allow = (options && options.allow) || []
-      const defaults = ["global", "eslint", "@ts-", "@typescript-eslint", "\\/"]
-      const re = new RegExp(String.raw`^\s?(${[...defaults, ...allow].join("|")})`)
+      const defaults = ['global', 'eslint', '@ts-', '@typescript-eslint', '\\/']
+      const re = new RegExp(String.raw`^\s?(${[...defaults, ...allow].join('|')})`)
       if (comment && !re.test(comment.value)) {
         context.report({
           fix(fixer) {
             return fixer.remove(comment)
           },
           loc: comment.loc,
-          message: "Comments are forbidden",
+          message: 'Comments are forbidden',
           node: null,
         })
       }
